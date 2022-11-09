fb1a004678adefdd9d10429e07ad4a617cbdf093
Pimed23 <b.pimed@gmail.com>
2022-11-08 21:45:25 -0500

    wip: Testing simple api

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
+import React, {useEffect, useState} from 'react';
 import './App.css';
-const backend = require('@code-as-service/server')
 
 function App() {
-  backend();
+  const [serverData, setServerData] = useState([{}]);
+  useEffect(() => {
+    fetch('/api').then(
+      response => response.json()
+    ).then(
+      data => {
+        setServerData(data);
+        console.log(data);
+      }
+    )
+  }, []);
+
   return (
     <div className="App">
-      Hello world
+      {(typeof serverData.users === 'undefined') ? (
+        <p>Cargando...</p>
+      ) : (
+        serverData.users.map((user, i) => {
+          return <p key={i}>{user}</p>
+        })
+      )
+    }
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-module.exports = () => console.log('Hello from back');
+const express = require('express');
+
+const app = express()
+
+app.get('/api', (req, res) => {
+    res.json({"users": ['example0', 'example1', 'example2']});
+});
+
+app.listen(5000, () => console.log('Server started on port 5000'));
